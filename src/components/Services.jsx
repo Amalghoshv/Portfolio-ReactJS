78264474645e6292a05a1f4b7b5a85a8efad6c45
Amalghoshv <amalghoshv@gmail.com>
2024-08-25 00:30:23 +0530

    Header UI Changes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,7 +4,7 @@ import "../styles/services.css";
 function Services() {
 
   return (
-    <div className="services-container">
+    <div className="services-container" data-aos="zoom-in" >
       <p className="services-subtitles">What I Do ?</p>
       <h5 className="titles">Services</h5>
       <div className="cards-container">
